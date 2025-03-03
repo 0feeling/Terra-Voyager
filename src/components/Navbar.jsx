8e65872a8f4f24560d570cc2437d5128d60a8b3b
Ofeeling <timothee.meunier@gmail.com>
2025-03-04 00:19:13 +0100

    design2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gradient-to-r from-blue-500 to-green-500 p-4 shadow-md">
+    <nav className="bg-gradient-to-bl from-green-300 to-blue-600 p-4 shadow-md">
       <div className="max-w-full mx-auto flex justify-between items-center">
         <div className="text-white text-2xl font-semibold flex items-center">
           {" "}
