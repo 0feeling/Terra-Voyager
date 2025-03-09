9074d589f2e8f0b733d759d68928b285f29d7f34
Ofeeling <timothee.meunier@gmail.com>
2025-03-09 02:27:43 +0100

    img prob resolution

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css"; // Import des styles de Leaflet
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Supprimer les icônes par défaut qui peuvent causer des problèmes de chemin
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
+});
 
 const MapComponent = ({ coordinates }) => {
   console.log("MapComponent rendu avec coordonnées :", coordinates);
